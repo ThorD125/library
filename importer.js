d5e02e7ca91212404e9c05e088dcb1195b839de3
ThorD125 <32074843+ThorD125@users.noreply.github.com>
2021-05-21 11:49:30 +0200

    fancy and slick

---
+++ importer.js
@@ -0,0 +1,8 @@
+import "./fancybox/jquery.fancybox.min.css"
+import "./fancybox/jquery.fancybox.min.js"
+
+
+import "./slick-slider/bulma.min.css"
+import "./slick-slider/slick.css"
+import "./slick-slider/slick-theme.css"
+import "./slick-slider/slick.min.js"
